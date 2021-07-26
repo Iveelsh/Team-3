dc7fb6de6279478c6a525bd64c181338bbfc133e
Bilguun2004 <bilguun8689@gmail.com>
2021-07-26 18:32:02 +0800

    sidebar

---
+++ js/sidebar.js
@@ -0,0 +1,49 @@
+
+        let taskb = document.getElementById("taskb");
+        let wishb = document.getElementById("wishb");
+        let groupb = document.getElementById("groupb");
+        let messb = document.getElementById("messb");
+        let task = document.getElementById("task");
+let wishlist = document.getElementById("wishlist");
+let groupinfo = document.getElementById("groupinfo");
+let message = document.getElementById("message");
+const displaytask = () =>{
+    task.classList.remove("none");
+    wishlist.classList.add("none");
+    groupinfo.classList.add("none");
+    message.classList.add("none");
+    taskb.classList.add("black");
+    wishb.classList.remove("black");
+    groupb.classList.remove("black");
+    messb.classList.remove("black")
+}
+const displaywishlist = () =>{
+    task.classList.add("none");
+    wishlist.classList.remove("none");
+    groupinfo.classList.add("none");
+    message.classList.add("none");
+    taskb.classList.remove("black");
+    wishb.classList.add("black");
+    groupb.classList.remove("black");
+    messb.classList.remove("black")
+}
+const displaygroupinfo = () =>{
+    task.classList.add("none");
+    wishlist.classList.add("none");
+    groupinfo.classList.remove("none");
+    message.classList.add("none");
+    taskb.classList.remove("black");
+    wishb.classList.remove("black");
+    groupb.classList.add("black");
+    messb.classList.remove("black")
+}
+const displaymessage = () =>{
+    task.classList.add("none");
+    wishlist.classList.add("none");
+    groupinfo.classList.add("none");
+    message.classList.remove("none");
+    taskb.classList.remove("black");
+    wishb.classList.remove("black");
+    groupb.classList.remove("black");
+    messb.classList.add("black")
+}
