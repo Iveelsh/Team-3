322a33c24bee35191dc592c61603c0fc0165e9a2
Tuvshintugs <tuvshintugs@nest-52s-iMac.local>
2021-08-11 19:05:15 +0800

    still not done

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,32 +1,46 @@
+let Dropdown_2 = document.getElementById("Dropdowns_2");
+let Dropdown = document.getElementById("Dropdowns");
+
 function myfunction() {
-  let Dropdown = document.getElementById("Dropdowns");
   let container = document.getElementById("dropdown-container");
   let btn = document.getElementById("Dropbtn");
 
   Dropdown.style.visibility = "visible";
+  Dropdown_2.style.visibility = "visible";
 }
 function showfunction() {
-  let Dropdown = document.getElementById("Dropdowns");
   let container = document.getElementById("dropdown-container");
 
   Dropdown.style.visibility = "hidden";
+  Dropdown_2.style.visibility = "hidden";
 }
 function dropfunction() {
-  let Dropdown = document.getElementById("Dropdowns");
   let container = document.getElementById("dropdown-container");
   let btn = document.getElementById("Dropbtn");
 
   Dropdown.style.visibility = "visible";
+  Dropdown_2.style.visibility = "visible";
 }
 function dropLeavefunction() {
-  let Dropdown = document.getElementById("Dropdowns");
   let container = document.getElementById("dropdown-container");
   let btn = document.getElementById("Dropbtn");
 
   Dropdown.style.visibility = "hidden";
+  Dropdown_2.style.visibility = "hidden";
 }
 
 let Home = document.getElementById("Home");
 let Wishlist = document.getElementById("Wishlist");
 let Settings = document.getElementById("Settings");
 let Tasks = document.getElementById("Tasks");
+
+let ustgah = document.getElementById("Wishlist");
+
+ustgah.addEventListener("onmouseover", () => {
+  ustgah.setAttribute("class", "red")
+}, false)
+ustgah.onmouseover = () => {
+  console.log("Aa")
+  ustgah.classList.add("red")
+
+}
