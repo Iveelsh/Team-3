f96991d16830d14786b550e8b96060ad694d0659
Tuvshintugs <tuvshintugs@nest-52s-iMac.local>
2021-08-12 18:46:09 +0800

    IM FCKING DONEEEEE

--- js/dropdown.js
+++ js/dropdown.js
@@ -39,8 +39,52 @@ let ustgah = document.getElementById("Wishlist");
 ustgah.addEventListener("onmouseover", () => {
   ustgah.setAttribute("class", "red")
 }, false)
-ustgah.onmouseover = () => {
-  console.log("Aa")
-  ustgah.classList.add("red")
 
+ustgah.onmouseover = () => {
+  document.getElementById("Ustgah").style.color = "red";
+  document.getElementById("Ustgah_2").style.color = "red";
+}
+
+ustgah.onmouseleave = () => {
+  document.getElementById("Ustgah").style.color = "white";
+  document.getElementById("Ustgah_2").style.color = "white";
+}
+
+let zasah = document.getElementsByClassName("Zasah")[0];
+
+zasah.onmouseover = () => {
+  document.getElementById("Zasah").style.color = "black";
+  document.getElementById("Zasah_2").style.color = "black";
+}
+
+zasah.onmouseleave = () => {
+  document.getElementById("Zasah").style.color = "white";
+  document.getElementById("Zasah_2").style.color = "white";
+}
+
+let x_cont = document.getElementById("X_container");
+let y_cont = document.getElementById("Y_container");
+
+x_cont.onmouseover = () => {
+  document.getElementById("X_container").style.backgroundColor = "#172662";
+  document.getElementById("circle").style.backgroundColor = "#172662";
+  document.getElementById("X").style.color = "white";
+}
+
+x_cont.onmouseleave = () => {
+  document.getElementById("X_container").style.backgroundColor = "white";
+  document.getElementById("circle").style.backgroundColor = "white";
+  document.getElementById("X").style.color = "#172662";
+}
+
+y_cont.onmouseover = () => {
+  document.getElementById("Y_container").style.backgroundColor = "#172662";
+  document.getElementById("circle_2").style.backgroundColor = "#172662";
+  document.getElementById("Y").style.color = "white";
+}
+
+y_cont.onmouseleave = () => {
+  document.getElementById("Y_container").style.backgroundColor = "white";
+  document.getElementById("circle_2").style.backgroundColor = "white";
+  document.getElementById("Y").style.color = "#172662";
 }
