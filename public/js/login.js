6fd648c43eaff4c7399b52cca782597402478abb
Iveel Shijir <iveel.sh@gmail.com>
2021-08-26 09:57:55 +0800

    pathchange

--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,7 @@ firebase.auth().onAuthStateChanged((user) => {
         db.collection("users").doc(uid).get().then((doc) => {
             let data = doc.data();
             if (!data.groupId) {
-                window.location.href = "groupAdd.html"
+                window.location.href = "groupCreateJoin.html"
             } else {
                 if (data.role === "admin") {
                     window.location.href = "home-parents.html"
